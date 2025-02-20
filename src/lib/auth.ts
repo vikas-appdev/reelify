72caff02458ef7d482f9ed7c99755c06e3fc92a9
Vikas Yadav <v.ray96610@gmail.com>
2025-02-20 20:15:33 +0530

    auth and middleware

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -40,5 +40,31 @@ export const authOptions: NextAuthOptions = {
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({token, user}) {
+            if(user){
+                token.id = user.id
+            }
+            return token;
+        },
+
+        async session({ session, token }) {
+            if(session.user){
+                session.user.id = token.id as string
+            }
+
+            return session;
+        }
+    },
+    pages: {
+        signIn: "/login",
+        error: "/login"
+    },
+    session:{
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    },
+    secret: process.env.SECRET
+
 }
